af39a0bc060dcc5ef7b5a124403c593ffb983356
Napan0s <vddvincenti@gmail.com>
2021-11-30 15:05:50 +0100

    Started implementing search page for events

--- lib/widgets/search_event_bottomsheet.dart
+++ lib/widgets/search_event_bottomsheet.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import '../themes/custom_colors.dart';
 
 class SearchEventBottomSheet extends StatelessWidget {
   const SearchEventBottomSheet({Key? key}) : super(key: key);
@@ -6,7 +8,32 @@ class SearchEventBottomSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: MediaQuery.of(context).size.height / 1.2,
+      color: onPrimary,
+      height: MediaQuery.of(context).size.height,
+      child: CustomScrollView(
+        slivers: [
+          SliverPadding(
+            padding: const EdgeInsets.only(top: 10),
+            sliver: SliverAppBar(
+              backgroundColor: onPrimary,
+              iconTheme: const IconThemeData(color: secondaryTextColor),
+              elevation: 2,
+              stretch: false,
+              toolbarHeight: MediaQuery.of(context).padding.top + 100,
+              shadowColor: Colors.black,
+              expandedHeight: 140,
+              primary: true,
+              systemOverlayStyle: const SystemUiOverlayStyle(
+                  statusBarColor: Colors.transparent),
+              title: const Text(
+                "Search",
+                style: TextStyle(color: secondaryTextColor),
+              ),
+              titleSpacing: 10,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
