8e364255f3b70cfe8437b517c20178424665a027
Napan0s <vddvincenti@gmail.com>
2021-12-18 12:46:43 +0100

    Add events kinda works, completing the http request.

--- lib/providers/events.dart
+++ lib/providers/events.dart
@@ -4,9 +4,10 @@ import 'package:flutter/material.dart';
 
 import './event.dart';
 import '../dummy_data/dummy_events.dart';
+import 'package:http/http.dart' as http;
 
 class Events with ChangeNotifier {
-  final List<Event> _items = [...dummy];
+  List<Event> _items = [];
 
   List<Event> get items {
     return [..._items];
@@ -16,10 +17,120 @@ class Events with ChangeNotifier {
     return _items.firstWhere((event) => event.id == id);
   }
 
-  Future<void> addEvent(Event event) async {
-    // final newEvent = Event(id: -1, createdAt: null
-    // );
-    // _items.add(newEvent);
-    // notifyListeners();
+  Future<void> fetchAndSetEvents() async {
+    final url = Uri.https(
+        'https://runwithme-c6e23-default-rtdb.europe-west1.firebasedatabase.app/',
+        '/events.json');
+    try {
+      final response = await http.get(url);
+      final extractedData = json.decode(response.body) as Map<String, dynamic>;
+      if (extractedData == null) {
+        return;
+      }
+      final List<Event> loadedEvents = [];
+      extractedData.forEach((eventId, eventData) {
+        loadedEvents.add(Event(
+          id: int.parse(eventId),
+          adminId: eventData['adminId'],
+          averageDuration: eventData['averageDuration'],
+          averageLength: eventData['averageLength'],
+          averagePace: eventData['averagePace'],
+          createdAt: eventData['createdAt'],
+          currentParticipants: eventData['currentParticipants'],
+          date: eventData['date'],
+          difficultyLevel: eventData['difficultyLevel'],
+          maxParticipants: eventData['maxParticipants'],
+          name: eventData['name'],
+          startingPintLat: eventData['startingPintLat'],
+          startingPintLong: eventData['startingPintLong'],
+        ));
+      });
+      _items = loadedEvents;
+      notifyListeners();
+    } catch (error) {
+      rethrow;
+    }
   }
+
+  Future<void> addEvent(Event event) async {
+    final url = Uri.https(
+        'runwithme-c6e23-default-rtdb.europe-west1.firebasedatabase.app',
+        '/events.json');
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode({
+          'adminId': event.adminId,
+          'averageDuration': event.averageDuration,
+          'averageLength': event.averageLength,
+          'averagePace': event.averagePace,
+          'createdAt': event.createdAt,
+          'currentParticipants': event.currentParticipants,
+          'date': event.date,
+          'difficultyLevel': event.difficultyLevel,
+          'maxParticipants': event.maxParticipants,
+          'name': event.name,
+          'startingPintLat': event.startingPintLat,
+          'startingPintLong': event.startingPintLong,
+        }),
+      );
+      final newEvent = Event(
+        adminId: event.adminId,
+        averageDuration: event.averageDuration,
+        averageLength: event.averageLength,
+        averagePace: event.averagePace,
+        createdAt: event.createdAt,
+        currentParticipants: event.currentParticipants,
+        date: event.date,
+        difficultyLevel: event.difficultyLevel,
+        maxParticipants: event.maxParticipants,
+        name: event.name,
+        startingPintLat: event.startingPintLat,
+        startingPintLong: event.startingPintLong,
+        id: json.decode(response.body)['id'],
+      );
+      _items.add(newEvent);
+      // _items.insert(0, newEvent); // at the start of the list
+      notifyListeners();
+    } catch (error) {
+      rethrow;
+    }
+  }
+
+  // Future<void> updateEvent(String id, Event newEvent) async {
+  //   final eventIndex = _items.indexWhere((event) => event.id == id);
+  //   if (eventIndex >= 0) {
+  //     final url = Uri.https(
+  //         'https://runwithme-c6e23-default-rtdb.europe-west1.firebasedatabase.app/',
+  //         '/Events/$id.json');
+  //     await http.patch(url,
+  //         body: json.encode({
+  //           'title': newEvent.title,
+  //           'description': newEvent.description,
+  //           'imageUrl': newEvent.imageUrl,
+  //           'price': newEvent.price
+  //         }));
+  //     _items[eventIndex] = newEvent;
+  //     notifyListeners();
+  //   } else {
+  //     print('...');
+  //   }
+  // }
+
+  // Future<void> deleteEvent(String id) async {
+  //   final url = Uri.https(
+  //       'https://runwithme-c6e23-default-rtdb.europe-west1.firebasedatabase.app/',
+  //       '/Events/$id.json');
+  //   final existingEventIndex = _items.indexWhere((event) => event.id == id);
+  //   var existingEvent = _items[existingEventIndex];
+  //   _items.removeAt(existingEventIndex);
+  //   notifyListeners();
+  //   final response = await http.delete(url);
+  //   if (response.statusCode >= 400) {
+  //     _items.insert(existingEventIndex, existingEvent);
+  //     notifyListeners();
+  //     throw HttpException('Could not delete Event.');
+  //   }
+  //   existingEvent = null;
+  // }
 }
---
+++ lib/providers/settings_manager.dart
@@ -0,0 +1,96 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:path_provider/path_provider.dart';
+import '../providers/color_scheme.dart';
+import '../classes/file_manager.dart';
+import 'package:flutter/foundation.dart';
+
+enum CustomThemeMode {
+  dark,
+  light,
+}
+
+enum NotificationMode {
+  all,
+  important,
+  off,
+}
+
+enum MapStyle {
+  light,
+  dark,
+}
+
+class UserSettings with ChangeNotifier {
+  String settingsFileName = 'settings.json';
+  bool location = false;
+  var settings = Settings();
+  CustomThemeMode mode = CustomThemeMode.light;
+  CustomColorScheme colors;
+
+  UserSettings({required this.colors});
+
+  void toggleLocalization() {
+    location = !location;
+    notifyListeners();
+  }
+
+  void setThemeMode(mode) {
+    print("here");
+    if (mode == CustomThemeMode.light) {
+      colors.setLightMode();
+    } else if (mode == CustomThemeMode.dark) {
+      colors.setDarkMode();
+    }
+    settings.theme = mode;
+    FileManager().writeFile(json.encode(settings.toJson()), settingsFileName);
+    notifyListeners();
+  }
+
+  Future<bool> loadSettings() async {
+    var settingsString = await FileManager().readFile(settingsFileName);
+    if (settingsString == 'Null') {
+      settingsString = json.encode(settings.toJson());
+      FileManager().writeFile(settingsString, settingsFileName);
+    }
+    settings.fromJson(jsonDecode(settingsString));
+
+    // ignore: todo
+    //TODO: This delay should be removed in production
+    await Future.delayed(const Duration(milliseconds: 2000));
+    if (settings.theme == CustomThemeMode.dark) {
+      colors.setDarkMode();
+    } else {
+      colors.setLightMode();
+    }
+    notifyListeners();
+    return true;
+  }
+}
+
+class Settings {
+  CustomThemeMode theme = CustomThemeMode.light;
+  bool location = false;
+  NotificationMode notification = NotificationMode.off;
+  MapStyle mapStyle = MapStyle.light;
+
+  fromJson(Map<String, dynamic> json) {
+    theme = CustomThemeMode.values
+        .firstWhere((e) => describeEnum(e) == json['theme']);
+    location = json['location'] == 'true';
+    notification = NotificationMode.values
+        .firstWhere((e) => describeEnum(e) == json['notification']);
+    mapStyle =
+        MapStyle.values.firstWhere((e) => describeEnum(e) == json['map_style']);
+  }
+
+  Map<String, dynamic> toJson() => {
+        'theme': theme.name,
+        'location': location.toString(),
+        'notification': notification.name,
+        'map_style': mapStyle.name,
+      };
+}
