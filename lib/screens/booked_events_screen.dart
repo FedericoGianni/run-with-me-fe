18e27b72213ba6e706a948167db36d1b2499197e
FedericoGianni <federicodeicas@gmail.com>
2022-03-20 15:44:21 +0100

    added loading animation in booked_events_screen

--- lib/screens/booked_events_screen.dart
+++ lib/screens/booked_events_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:runwithme/providers/event.dart';
 import 'package:runwithme/providers/settings_manager.dart';
 import 'package:runwithme/widgets/permissions_message.dart';
+import 'package:runwithme/widgets/splash.dart';
 
 import '../providers/events.dart';
 import '../providers/user.dart';
@@ -80,119 +81,124 @@ class _BookedEventsScreenState extends State<BookedEventsScreen> {
       if (_rowColor == Colors.deepOrange.shade900) {
         __selectGridView(colors);
       }
-      return CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            stretch: false,
-            toolbarHeight: 123,
-            title: Container(
-              color: colors.onPrimary,
-              height: 123,
-              width: double.infinity,
+
+      if (_isLoading) {
+        return SplashScreen();
+      } else {
+        return CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              stretch: false,
+              toolbarHeight: 123,
+              title: Container(
+                color: colors.onPrimary,
+                height: 123,
+                width: double.infinity,
+                padding: const EdgeInsets.only(
+                  top: 50,
+                  bottom: 20,
+                ),
+                child: Image.asset(
+                  "assets/icons/logo_gradient.png",
+                  width: 130,
+                ),
+              ),
+
+              titleSpacing: 0,
+              expandedHeight: 183,
+              backgroundColor: colors.background,
+
+              // back up the list of items.
+              floating: true,
+              // Display a placeholder widget to visualize the shrinking size.
+              pinned: true,
+              snap: false,
+              elevation: 2,
+              flexibleSpace: FlexibleSpaceBar(
+                title: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Text(
+                      events.bookedEvents.length.toString() + " results",
+                      style: TextStyle(
+                          color: colors.secondaryTextColor, fontSize: 10),
+                    ),
+                    Row(
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.table_rows_outlined),
+                          color: _rowColor,
+                          onPressed: () {
+                            __selectListView(colors);
+                          },
+                          padding: EdgeInsets.zero,
+                          constraints: const BoxConstraints(),
+                          splashRadius: 10,
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.grid_view_outlined),
+                          color: _gridColor,
+                          onPressed: () {
+                            __selectGridView(colors);
+                          },
+                          padding: EdgeInsets.zero,
+                          constraints: const BoxConstraints(),
+                          splashRadius: 10,
+                        ),
+                      ],
+                    ),
+                  ],
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                ),
+                titlePadding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+              ),
+              // Make the initial height of the SliverAppBar larger than normal.
+            ),
+            SliverPadding(
               padding: const EdgeInsets.only(
-                top: 50,
-                bottom: 20,
-              ),
-              child: Image.asset(
-                "assets/icons/logo_gradient.png",
-                width: 130,
-              ),
-            ),
-
-            titleSpacing: 0,
-            expandedHeight: 183,
-            backgroundColor: colors.background,
-
-            // back up the list of items.
-            floating: true,
-            // Display a placeholder widget to visualize the shrinking size.
-            pinned: true,
-            snap: false,
-            elevation: 2,
-            flexibleSpace: FlexibleSpaceBar(
-              title: Row(
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  Text(
-                    events.bookedEvents.length.toString() + " results",
-                    style: TextStyle(
-                        color: colors.secondaryTextColor, fontSize: 10),
-                  ),
-                  Row(
-                    children: [
-                      IconButton(
-                        icon: const Icon(Icons.table_rows_outlined),
-                        color: _rowColor,
-                        onPressed: () {
-                          __selectListView(colors);
-                        },
-                        padding: EdgeInsets.zero,
-                        constraints: const BoxConstraints(),
-                        splashRadius: 10,
-                      ),
-                      IconButton(
-                        icon: const Icon(Icons.grid_view_outlined),
-                        color: _gridColor,
-                        onPressed: () {
-                          __selectGridView(colors);
-                        },
-                        padding: EdgeInsets.zero,
-                        constraints: const BoxConstraints(),
-                        splashRadius: 10,
-                      ),
-                    ],
-                  ),
-                ],
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              ),
-              titlePadding:
-                  const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
-            ),
-            // Make the initial height of the SliverAppBar larger than normal.
-          ),
-          SliverPadding(
-            padding:
-                const EdgeInsets.only(bottom: 0, top: 20, left: 20, right: 20),
-            sliver: SliverToBoxAdapter(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Booked events",
-                    style: TextStyle(
-                        color: colors.primaryTextColor,
-                        fontSize: 20,
-                        fontWeight: FontWeight.w900),
-                  ),
-                ],
+                  bottom: 0, top: 20, left: 20, right: 20),
+              sliver: SliverToBoxAdapter(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Booked events",
+                      style: TextStyle(
+                          color: colors.primaryTextColor,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w900),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          SliverPadding(
-            padding:
-                const EdgeInsets.only(bottom: 40, top: 20, left: 20, right: 20),
-            sliver: SliverGrid(
-              gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                childAspectRatio: _aspectRatio,
-                mainAxisSpacing: 15.0,
-                crossAxisSpacing: 15.0,
-                maxCrossAxisExtent: 400 / _view,
-              ),
-              delegate: SliverChildBuilderDelegate(
-                (context, index) {
-                  return EventItem(
-                    events.bookedEvents[index],
-                    index,
-                    events.bookedEvents.length,
-                  );
-                },
-                childCount: events.bookedEvents.length,
+            SliverPadding(
+              padding: const EdgeInsets.only(
+                  bottom: 40, top: 20, left: 20, right: 20),
+              sliver: SliverGrid(
+                gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                  childAspectRatio: _aspectRatio,
+                  mainAxisSpacing: 15.0,
+                  crossAxisSpacing: 15.0,
+                  maxCrossAxisExtent: 400 / _view,
+                ),
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                    return EventItem(
+                      events.bookedEvents[index],
+                      index,
+                      events.bookedEvents.length,
+                    );
+                  },
+                  childCount: events.bookedEvents.length,
+                ),
               ),
             ),
-          ),
-          // Next, create a SliverList
-        ],
-      );
+            // Next, create a SliverList
+          ],
+        );
+      }
     } else {
       return PermissionMessage();
     }
