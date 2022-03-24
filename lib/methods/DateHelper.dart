a7d378ebc7d20b13cc09218854ce8728c9d19258
FedericoGianni <federico.deicas@mail.polimi.it>
2022-03-24 23:48:39 +0100

    working weekly stats in homepage

--- lib/methods/DateHelper.dart
+++ lib/methods/DateHelper.dart
@@ -4,4 +4,13 @@ class DateHelper {
         ? "0" + input.toString()
         : input.toString();
   }
+
+  static int diffInDays(DateTime date1, DateTime date2) {
+    return ((date1.difference(date2) -
+                    Duration(hours: date1.hour) +
+                    Duration(hours: date2.hour))
+                .inHours /
+            24)
+        .round();
+  }
 }
