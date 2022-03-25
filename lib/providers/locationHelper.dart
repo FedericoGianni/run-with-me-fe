3d00b233942042ccd0ee0bba309ad5968b1ec11c
Napan0s <vddvincenti@gmail.com>
2022-03-25 18:10:31 +0100

    Added back to home function to all pages

--- lib/providers/locationHelper.dart
+++ lib/providers/locationHelper.dart
@@ -11,7 +11,7 @@ import '../widgets/custom_alert_dialog.dart';
 class LocationHelper with ChangeNotifier {
   var context;
   bool searchingForLocation = false;
-  bool locationEnableRequested = false;
+  bool keepBackgroundThreadAlive = false;
   late Map<String, Object> _place;
   String kPLACES_API_KEY = "************REMOVED************";
 
@@ -69,105 +69,86 @@ class LocationHelper with ChangeNotifier {
     );
   }
 
+  Future<void> start() async {
+    print("Starting background location thread");
+    keepBackgroundThreadAlive = true;
+    while (keepBackgroundThreadAlive) {
+      print("Searching for user location in the background");
+      await Future.delayed(Duration(seconds: 30));
+      determinePosition(LocationAccuracy.best, context);
+    }
+    print("Closing background location thread");
+  }
+
   Future<Position> determinePosition(
       LocationAccuracy accuracy, var context) async {
-    if (!searchingForLocation) {
-      searchingForLocation = true;
+    bool serviceEnabled;
+    LocationPermission permission;
+    // Here I check if location services are enabled
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
 
-      bool serviceEnabled;
-      LocationPermission permission;
-      // Here I check if location services are enabled
-      serviceEnabled = await Geolocator.isLocationServiceEnabled();
-
-      // If not, the corresponding alert dialog is triggered
-      if (!serviceEnabled) {
-        // But only the first time
-        if (!locationEnableRequested) {
-          locationEnableRequested = true;
-          await _showMyDialog(
-              context: context,
-              title: "No GPS data",
-              message:
-                  "Location services are disabled but are needed for the app to work at its best.\nDo you want to turn them on now?",
-              onAccept: () {
-                Geolocator.openLocationSettings();
-                // all again this function that will attewmpt 10 check before closing itself too
-                determinePosition(LocationAccuracy.best, context);
-                // For the time being lets use the default user position, hoping for the best in the future
-                searchingForLocation = false;
-                return _getDefaultUserLocation();
-              },
-              onDismiss: () {
-                // If user selected no, default user position is going to be used instead
-                searchingForLocation = false;
-                return _getDefaultUserLocation();
-              });
-          print('Location services are disabled.');
-          return _getDefaultUserLocation();
-        } else {
-          for (var i = 0; i < 5; i++) {
-            await Future.delayed(Duration(seconds: 1));
-            print("Re checking if location services are enabled");
-            serviceEnabled = await Geolocator.isLocationServiceEnabled();
-            if (serviceEnabled) {
-              print("Breaking cos location services are now active");
-              break;
-            }
-          }
-          // If after 10 tries the user still hasnt turned on the location, the default location is used instead
-          if (!serviceEnabled) {
-            print('Location services are disabled.');
-            searchingForLocation = false;
+    // If not, the corresponding alert dialog is triggered
+    if (!serviceEnabled) {
+      // But only the first time
+      await _showMyDialog(
+          context: context,
+          title: "No GPS data",
+          message:
+              "Location services are disabled but are needed for the app to work at its best.\nDo you want to turn them on now?",
+          onAccept: () {
+            Geolocator.openLocationSettings();
+            // all again this function that will attewmpt 10 check before closing itself too
+            determinePosition(LocationAccuracy.best, context);
+            // For the time being lets use the default user position, hoping for the best in the future
             return _getDefaultUserLocation();
-          }
-        }
-      }
+          },
+          onDismiss: () {
+            // If user selected no, default user position is going to be used instead
+            return _getDefaultUserLocation();
+          });
+      print('Location services are disabled.');
+      return _getDefaultUserLocation();
+    }
+    // If after 10 tries the user still hasnt turned on the location, the default location is used instead
 
-      permission = await Geolocator.checkPermission();
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      // If location permissions are denied the used is asked to allow them
+      await _showMyDialog(
+          context: context,
+          title: "No GPS permission",
+          message:
+              "Location services are used to search for events near you.\n\nDo you want to allow them now?",
+
+          // If he says yes, he is promted again with the default android permission manager
+          onAccept: () async {
+            permission = await Geolocator.requestPermission();
+          },
+          onDismiss: () {
+            // If user selected no, default user position is going to be used instead
+            return _getDefaultUserLocation();
+          });
       if (permission == LocationPermission.denied) {
-        // If location permissions are denied the used is asked to allow them
-        await _showMyDialog(
-            context: context,
-            title: "No GPS permission",
-            message:
-                "Location services are used to search for events near you.\n\nDo you want to allow them now?",
-
-            // If he says yes, he is promted again with the default android permission manager
-            onAccept: () async {
-              permission = await Geolocator.requestPermission();
-            },
-            onDismiss: () {
-              // If user selected no, default user position is going to be used instead
-              searchingForLocation = false;
-              return _getDefaultUserLocation();
-            });
-        if (permission == LocationPermission.denied) {
-          print('Location permissions are denied');
-          searchingForLocation = false;
-          return _getDefaultUserLocation();
-        }
-      }
-
-      if (permission == LocationPermission.deniedForever) {
-        print(
-            'Location permissions are permanently denied, we cannot request permissions.');
-        searchingForLocation = false;
+        print('Location permissions are denied');
         return _getDefaultUserLocation();
       }
-      print("Getting Location");
-      _currentUserPosition = await Geolocator.getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.best,
-          timeLimit: Duration(seconds: 10));
-
-      _lastKnownPosition = _currentUserPosition;
-      print("Location acquired");
-      notifyListeners();
-      searchingForLocation = false;
-      return _currentUserPosition;
-    } else {
-      print("already searching for user location");
-      return _lastKnownPosition;
     }
+
+    if (permission == LocationPermission.deniedForever) {
+      keepBackgroundThreadAlive = false;
+      print(
+          'Location permissions are permanently denied, we cannot request permissions.');
+      return _getDefaultUserLocation();
+    }
+    print("Getting Location");
+    _currentUserPosition = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.best,
+        timeLimit: Duration(seconds: 10));
+
+    _lastKnownPosition = _currentUserPosition;
+    print("Location acquired");
+    notifyListeners();
+    return _currentUserPosition;
   }
 
   double getDistanceBetween(
