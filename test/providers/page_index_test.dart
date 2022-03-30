08513cf0ffaad346d9ce8200315cde490e7ff1ea
FedericoGianni <federico.deicas@mail.polimi.it>
2022-03-30 12:16:53 +0200

    page index unit test

---
+++ test/providers/page_index_test.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:runwithme/providers/page_index.dart';
+
+void main() {
+  PageIndex pageIndex = PageIndex();
+
+  group('[PAGE INDEX]]', () {
+    test('pageIndex is set correctly', () {
+      pageIndex.setPage(Screens.HOME.index);
+      expect(pageIndex.index, Screens.HOME.index);
+
+      pageIndex.setPage(Screens.SEARCH.index);
+      expect(pageIndex.index, Screens.SEARCH.index);
+
+      pageIndex.setPage(Screens.NEW.index);
+      expect(pageIndex.index, Screens.NEW.index);
+
+      pageIndex.setPage(Screens.EVENTS.index);
+      expect(pageIndex.index, Screens.EVENTS.index);
+
+      pageIndex.setPage(Screens.USER.index);
+      expect(pageIndex.index, Screens.USER.index);
+    });
+  });
+}
