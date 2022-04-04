82cf0d0618d976389c6a15cfba9360301277c3d6
FedericoGianni <federico.deicas@mail.polimi.it>
2022-04-04 14:41:01 +0200

    add gradient app bar widget test

---
+++ test/widgets/gradientAppbar_test.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+import 'package:runwithme/providers/color_scheme.dart';
+import 'package:runwithme/providers/events.dart';
+import 'package:runwithme/providers/locationHelper.dart';
+import 'package:runwithme/providers/page_index.dart';
+import 'package:runwithme/providers/settings_manager.dart';
+import 'package:runwithme/providers/user.dart';
+import 'package:runwithme/widgets/gradientAppbar.dart';
+
+void main() {
+  testWidgets('[GRADIENT APP BAR]', (WidgetTester tester) async {
+    void setScreenSize({required int width, required int height}) {
+      final dpi = tester.binding.window.devicePixelRatio;
+      tester.binding.window.physicalSizeTestValue =
+          Size(width * dpi, height * dpi);
+    }
+
+    // used because deafult test widget env is 400x600 so it overflows
+    //setScreenSize(width: 1080, height: 2000);
+
+    List<Widget> _widgetList = [];
+
+    await tester.pumpWidget(
+      MultiProvider(
+          providers: [
+            ChangeNotifierProvider<CustomColorScheme>.value(
+                value: CustomColorScheme()),
+            ChangeNotifierProvider<Events>.value(value: Events()),
+            ChangeNotifierProvider<LocationHelper>.value(
+              value: LocationHelper(),
+            ),
+            ChangeNotifierProvider<UserSettings>.value(
+              value: UserSettings(),
+            ),
+            ChangeNotifierProvider<PageIndex>.value(
+              value: PageIndex(),
+            ),
+            ChangeNotifierProvider<User>.value(
+              value: User(),
+            ),
+          ],
+          child: MaterialApp(
+              home: Material(
+            child: GradientAppBar(200, _widgetList),
+          ))),
+    );
+
+    expect(find.byType(Container), findsOneWidget);
+  });
+}
