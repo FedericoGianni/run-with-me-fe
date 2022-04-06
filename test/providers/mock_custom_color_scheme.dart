977e2d3f7e5068c871891a31b54ec1002bdce9ec
FedericoGianni <federico.deicas@mail.polimi.it>
2022-04-06 11:53:56 +0200

    added mock providers and more add event screen tests

---
+++ test/providers/mock_custom_color_scheme.dart
@@ -0,0 +1,9 @@
+import 'package:mockito/mockito.dart';
+import 'package:runwithme/providers/color_scheme.dart';
+import 'package:runwithme/providers/event.dart';
+import 'package:runwithme/providers/events.dart';
+import 'package:runwithme/models/dummy_events.dart';
+import 'package:runwithme/providers/settings_manager.dart';
+import 'package:runwithme/providers/user.dart';
+
+class MockCustomColorScheme extends Mock implements CustomColorScheme {}
