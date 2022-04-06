2ab2824d434db193a59e8de98fe12d1100dcdc17
FedericoGianni <federico.deicas@mail.polimi.it>
2022-04-06 11:53:56 +0200

    added mock providers and more add event screen tests

---
+++ test/providers/mock_user_settings_provider.dart
@@ -0,0 +1,21 @@
+import 'package:mockito/mockito.dart';
+import 'package:runwithme/providers/event.dart';
+import 'package:runwithme/providers/events.dart';
+import 'package:runwithme/models/dummy_events.dart';
+import 'package:runwithme/providers/settings_manager.dart';
+import 'package:runwithme/providers/user.dart';
+
+class MockUserSettingsProvider extends Mock implements UserSettings {
+  @override
+  Settings settings = Settings();
+
+  @override
+  bool isLoggedIn() {
+    return settings.isLoggedIn;
+  }
+
+  @override
+  void setisLoggedIn(bool set) {
+    settings.isLoggedIn = set;
+  }
+}
