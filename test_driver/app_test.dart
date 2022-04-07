7ddaafc423f391b6a18e66b16ac7cd9acdd8ab3a
FedericoGianni <federico.deicas@mail.polimi.it>
2022-04-07 12:19:22 +0200

    add integration test

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -4,14 +4,20 @@ import 'package:integration_test/integration_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:provider/provider.dart';
 
-import 'package:runwithme/main.dart' as app;
 import 'package:runwithme/providers/color_scheme.dart';
 import 'package:runwithme/providers/events.dart';
 import 'package:runwithme/providers/locationHelper.dart';
 import 'package:runwithme/providers/page_index.dart';
 import 'package:runwithme/providers/settings_manager.dart';
 import 'package:runwithme/providers/user.dart';
+import 'package:runwithme/screens/add_event_screen.dart';
+import 'package:runwithme/screens/booked_events_screen.dart';
+import 'package:runwithme/screens/event_details_screen.dart';
 import 'package:runwithme/screens/home_screen.dart';
+import 'package:runwithme/screens/search_screen.dart';
+import 'package:runwithme/screens/tabs_screen.dart';
+import 'package:runwithme/screens/user_screen.dart';
+import 'package:runwithme/widgets/permissions_message.dart';
 
 // MOCK PROVIDERS
 import '../test/providers/mock_events_provider.dart';
@@ -29,8 +35,6 @@ class MockBuildContext extends Mock implements BuildContext {
 }
 
 void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-
   group('end-to-end test', () {
     testWidgets('app', (WidgetTester tester) async {
       // MOCK CONTEXT
@@ -46,7 +50,9 @@ void main() {
       // used because deafult test widget env is 400x600 so it overflows
       setScreenSize(width: 1080, height: 1920);
 
-      await tester.pumpWidget(MultiProvider(
+      // MOCK MAIN cause I need to put mock providers inside
+      await tester.pumpWidget(
+        MultiProvider(
           providers: [
             ChangeNotifierProvider<CustomColorScheme>.value(
                 value: CustomColorScheme()),
@@ -64,19 +70,103 @@ void main() {
               value: mockUser,
             ),
           ],
-          child: MaterialApp(onGenerateRoute: (settings) {
-            return MaterialPageRoute(
-              settings:
-                  // event detail gets event from route settings.arguments
-                  const RouteSettings(),
-              builder: (context) {
-                return Material(
-                    child: MediaQuery(
-                        data: const MediaQueryData(textScaleFactor: 0.5),
-                        child: HomeScreen()));
+          child: MaterialApp(
+              initialRoute: '/', // default is '/'
+              routes: {
+                '/': (ctx) => TabsScreen(),
+                AddEventScreen.routeName: (ctx) => AddEventScreen(),
+                BookedEventsScreen.routeName: (ctx) => BookedEventsScreen(),
+                UserScreen.routeName: (ctx) => UserScreen(),
+                HomeScreen.routeName: (ctx) => HomeScreen(),
+                EventDetailsScreen.routeName: (ctx) =>
+                    const EventDetailsScreen(),
+                SearchScreen.routeName: (ctx) => const EventDetailsScreen(),
               },
-            );
-          })));
+              onGenerateRoute: (settings) {
+                return MaterialPageRoute(
+                  settings:
+                      // event detail gets event from route settings.arguments
+                      const RouteSettings(),
+                  builder: (context) {
+                    return Material(
+                        child: MediaQuery(
+                            data: const MediaQueryData(textScaleFactor: 0.5),
+                            child: HomeScreen()));
+                  },
+                );
+              }),
+        ),
+      );
+
+      // user should not be logged
+      expect(mockUserSettingsProvider.isLoggedIn(), false);
+
+      // TABS SCREEN
+      // now the app is first set to tabs screen
+      expect(find.byType(BottomNavigationBar), findsOneWidget);
+
+      TabsScreen tabsScreen =
+          find.byType(TabsScreen).first.evaluate().single.widget as TabsScreen;
+
+      // SEARCH SCREEN
+      // now let's switch to search screen
+      tester
+          .state<TabsScreenState>(find.byType(TabsScreen))
+          .selectPage(Screens.SEARCH.index);
+
+      await tester.pump();
+      expect(find.byType(SearchScreen), findsOneWidget);
+
+      // BOOKED EVENTS SCREEN
+      // now let's switch to booked event screen
+      tester
+          .state<TabsScreenState>(find.byType(TabsScreen))
+          .selectPage(Screens.EVENTS.index);
+
+      await tester.pump();
+      expect(find.byType(BookedEventsScreen), findsOneWidget);
+
+      // since we are not logged in we should find Permission Message
+      await tester.pump();
+      expect(find.byType(PermissionMessage), findsOneWidget);
+
+      // ADD EVENT SCREEN
+      // now let's switch to add event screen
+      tester
+          .state<TabsScreenState>(find.byType(TabsScreen))
+          .selectPage(Screens.NEW.index);
+
+      await tester.pump();
+      expect(find.byType(AddEventScreen), findsOneWidget);
+
+      // since we are not logged in we should find Permission Message
+      await tester.pump();
+      expect(find.byType(PermissionMessage), findsOneWidget);
+
+      // USER SCREEN
+      // now let's switch to user screen
+      tester
+          .state<TabsScreenState>(find.byType(TabsScreen))
+          .selectPage(Screens.USER.index);
+
+      await tester.pump();
+      expect(find.byType(UserScreen), findsOneWidget);
+
+      // now let's set login to true and rebuild everything
+      mockUserSettingsProvider.setisLoggedIn(true);
+      expect(mockUserSettingsProvider.isLoggedIn(), true);
+      await tester.pump();
+
+      // BOOKED EVENTS SCREEN (+login)
+      // now let's switch to booked event screen while logged in
+      tester
+          .state<TabsScreenState>(find.byType(TabsScreen))
+          .selectPage(Screens.EVENTS.index);
+
+      await tester.pump();
+      expect(find.byType(BookedEventsScreen), findsOneWidget);
+
+      // }
     });
   });
 }
